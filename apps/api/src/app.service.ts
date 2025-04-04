de94ec77cb0e4f98ed1a4bbd8495c60374e6bb80
Santiago Torres <torres.kilmes@gmail.com>
2025-04-04 02:39:28 -0300

    feat: update greeting message in AppService to include additional text

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello TimerTick API !!!!!!!!!!!!!! \n Que bueno que está esto \n si la verdad god';
+    return 'Hello TimerTick API !!!!!!!!!!!!!! \n Que bueno que está esto \n si la verdad god \n Sep';
   }
 }
