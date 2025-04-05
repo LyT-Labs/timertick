4e33580d8576b56d6b0399e2e82e907c997c82a5
Santiago Torres <torres.kilmes@gmail.com>
2025-04-05 10:05:28 -0300

    feat: restore original greeting message in AppService

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    // return 'Hello TimerTick API ! \n Que bueno que está esto \n si la verdad god \n Sep =)';
-    return 'Hello TimerTick API !!!!!!! <3';
+    return 'Hello TimerTick API ! \n Que bueno que está esto \n si la verdad god \n Sep =)';
+    // return 'Hello TimerTick API !!!!!!! <3';
   }
 }
